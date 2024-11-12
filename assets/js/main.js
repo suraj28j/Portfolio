fce3268102f76b4f95e8ea49c9194b56275755c4
Suraj Kumar <suraj28j@gmail.com>
2024-11-13 00:33:38 +0530

    about section update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let navList = document.getElementById("navList");
 let hamburger = document.getElementById("hamburger");
-let header = document.getElementsByTagName("header");
+let introduction = document.getElementById("introduction");
+// let main = document.getElementsByTagName("main");
 
 let click = false;
 
@@ -8,8 +9,17 @@ hamburger.addEventListener('click', () => {
     if (click === false) {
         navList.style.display = 'block';
         click = true;
+        introduction.style.marginTop = '250px';
     } else {
         navList.style.display = 'none'
         click = false;
+        introduction.style.marginTop = '100px';
+        window.scrollTo(0,0);
     }
 })
+
+
+// let nav_about = document.getElementById("nav_about");
+// nav_about.addEventListener('click',()=>{
+//     window.scrollTo(0,100);
+// })
