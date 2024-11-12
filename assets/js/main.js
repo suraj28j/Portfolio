78cd26cfa37f84488111eefdcf459e86c5a3c32e
Suraj Kumar <suraj28j@gmail.com>
2024-11-12 11:35:04 +0530

    media query

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
 let navList = document.getElementById("navList");
 let hamburger = document.getElementById("hamburger");
+let header = document.getElementsByTagName("header");
 
 let click = false;
 
 hamburger.addEventListener('click', () => {
     if (click === false) {
-        navList.style.visibility = 'visible';
+        navList.style.display = 'block';
         click = true;
     } else {
-        navList.style.visibility = 'hidden';
+        navList.style.display = 'none'
         click = false;
     }
 })
