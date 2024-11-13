3bd7307b55c0655b6862613d1c07cea25bbbfa26
Suraj Kumar <suraj28j@gmail.com>
2024-11-13 18:14:43 +0530

    media query update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 let navList = document.getElementById("navList");
 let hamburger = document.getElementById("hamburger");
-let introduction = document.getElementById("introduction");
-// let main = document.getElementsByTagName("main");
 
 let click = false;
 
@@ -9,17 +7,9 @@ hamburger.addEventListener('click', () => {
     if (click === false) {
         navList.style.display = 'block';
         click = true;
-        // introduction.style.marginTop = '250px';
     } else {
         navList.style.display = 'none'
         click = false;
-        // introduction.style.marginTop = '100px';
-        // window.scrollTo(0,0);
     }
 })
 
-
-// let nav_about = document.getElementById("nav_about");
-// nav_about.addEventListener('click',()=>{
-//     window.scrollTo(0,100);
-// })
