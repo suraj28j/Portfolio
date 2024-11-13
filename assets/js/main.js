bd9ed893a887b0f1bfdff28e3588cf5689036e99
Suraj Kumar <suraj28j@gmail.com>
2024-11-13 22:26:45 +0530

    about section animation add

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,26 @@ hamburger.addEventListener('click', () => {
     }
 })
 
+
+let nav_home = document.getElementById("nav_home");
+let nav_about = document.getElementById("nav_about");
+let nav_services = document.getElementById("nav_services");
+// let nav_projects = document.getElementById("nav_projects");
+// let nav_contact = document.getElementById("nav_contact");
+
+
+nav_home.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+nav_about.addEventListener('click', () => {
+    window.scrollTo(0, 800);
+})
+nav_services.addEventListener('click', () => {
+    window.scrollTo(0, 1600);
+})
+// nav_projects.addEventListener('click', () => {
+//     window.scrollTo(0, 2400);
+// })
+// nav_contact.addEventListener('click', () => {
+//     window.scrollTo(0, 3200);
+// })
